e7b5f447c51ec4318759ca95f8c489a20e4c5c84
Shantanu Gupta <Gupta.Shantanu6@gmail.com>
2020-09-26 21:32:39 +0530

    Added a Nav Bar

--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -5,8 +5,10 @@ import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
@@ -22,12 +24,28 @@ import java.lang.NullPointerException
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var toggle: ActionBarDrawerToggle
+
     var currentImageUrl : String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         loadmeme()
+
+        toggle = ActionBarDrawerToggle(this,drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.newMeme -> loadmeme()
+                R.id.savedMeme -> loadmeme()
+            }
+            true
+        }
     }
 
     private fun loadmeme(){
@@ -80,6 +98,14 @@ class MainActivity : AppCompatActivity() {
 
     fun nextMeme(view: View) {
         loadmeme()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        
+        return super.onOptionsItemSelected(item)
     }
 
 }
